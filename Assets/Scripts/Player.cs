a62412b5a069155e43a804ff37d7ff0a1d0a43db
Sviatoslav <svyarku@tlu.ee>
2018-10-31 19:44:17 +0200

    Added player movement and bullet mesh

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    //members or variables
+    public float moveSpeed = 7.0f;
+
+    //mehtods or functions
+
+	
+	// Update is called once per frame
+	void Update () {
+        //player movement
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+
+        }
+	}
+}
